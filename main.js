3cc9052158edf775489ea785e9005acf42cb338e
aarav010710 <76567151+aarav010710@users.noreply.github.com>
2021-04-16 15:28:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function monday() {
+    document.getElementById('lbmonday').innerHTML = English, Maths, Science, SST
+}
+
+function tuesday() {
+    document.getElementById('lbtuesday').innerHTML = English, Maths, Hindi, SST
+}
+
+function wednesday() {
+    document.getElementById('lbtwednesday').innerHTML = English, Maths, Science, SST
+}
+
+function thursday() {
+    document.getElementById('lbthursday').innerHTML = English, Maths, Science, SST
+}
+
+function friday() {
+    document.getElementById('lbfriday').innerHTML = English, Maths, Science, Hindi
+}
